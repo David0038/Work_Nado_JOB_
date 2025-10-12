a8244a933ecf39271fe5e753d07243cc446ddf55
David0038 <davidnazarov246@gmail.com>
2025-10-12 20:33:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import uvicorn
 
 # ✅ Настройки из переменных окружения (Render использует ENV)
 API_TOKEN = os.getenv("API_TOKEN", "8394026180:AAEHHKn30U7H_zdHWGu_cB2h9054lmo1eag")
-YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID", "test_J8y43wGt8go7fyMtkNNWUGlMdTmVtV41bd82cVmMpQk")
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID", "test_1179735")
 YOOKASSA_SECRET = os.getenv("YOOKASSA_SECRET", "live_CHbCwtxJJFbbt1V1jc14k_Pwgp_f-X6AqkO5_skAEbc")
 
 bot = Bot(token=API_TOKEN)
